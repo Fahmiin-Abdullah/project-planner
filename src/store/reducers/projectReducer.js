a235a2b0d617b3ef0e30c3eed074a82ae89a4556
fahmiin <fahmiin@tyne-solutions.com>
2020-06-19 01:12:20 +0800

    Added dummy projects data and display in ProjectList

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    { id: 1, title: 'help me find some peaches', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, aut!' },
+    { id: 2, title: 'collect all the honey stars', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, aut!' },
+    { id: 3, title: 'egg hunting with mom', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, aut!' }
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state
