ee133520bae5b40896056f35eafe8e5862a06ac0
fahmiin <fahmiin@tyne-solutions.com>
2020-06-23 22:30:35 +0800

    Output correct content in ProjectSummary component

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,8 +1,11 @@
 export const createProject = project => {
-  return (dispatch, getState, { getFirebase, getFirestore }) => {
+  return (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore()
+    const profile = getState().firebase.profile
+    const authorId = getState().firebase.auth.uid
+
     firestore.collection('projects').add({
-      ...project, authorFirstName: 'Fahmiin', authorLastName: 'Abdullah', authorId: 12345, createdAt: new Date()
+      ...project, authorFirstName: profile.firstName, authorLastName: profile.lastName, authorId, createdAt: new Date()
     })
     .then(() => dispatch({ type: 'CREATE_PROJECT', project }))
     .catch(err => dispatch({ type: 'CREATE_PROJECT_ERROR', err }))
